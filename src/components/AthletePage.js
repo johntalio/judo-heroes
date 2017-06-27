11704682d735cea18292d9f7ff79089d66419383
John Talio <johntalio@yahoo.com>
2017-06-27 19:48:09 -0400

    Add medal count info

--- src/components/AthletePage.js
+++ src/components/AthletePage.js
@@ -27,6 +27,12 @@ export default class AthletePage extends React.Component {
             Olympic medalist from <strong><Flag code={athlete.country} showName="true"/></strong>,
             born in {athlete.birth} (Find out more on <a href={athlete.link} target="_blank">Wikipedia</a>).
           </section>
+          <section className="medals">
+            <p>Winner of <strong>{athlete.medals.length}</strong> medals:</p>
+            <ul>{
+              athlete.medal.map((medal, i) => <Medal key={i} {...medal} />)
+            }</ul>
+          </section>
         </div>
         <div className="navigateBack">
           <Link to="/">« Back to the index</Link>
