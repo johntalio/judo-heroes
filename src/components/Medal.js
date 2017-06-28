8b515d23dd984ce923b45057959f21bc3253f310
John Talio <johntalio@yahoo.com>
2017-06-28 10:28:52 -0400

    Small fixes to format

--- src/components/Medal.js
+++ src/components/Medal.js
@@ -10,8 +10,8 @@ export default class Medal extends React.Component {
   render() {
     return(
       <li className="medal">
-        <span className={`symbol-${this.props.type}`} title={typeMap[this.props.type]}>{this.props.type}</span>
-        <span className="year">{this.props.year}</span>
+        <span className={`symbol symbol-${this.props.type}`} title={typeMap[this.props.type]}>{this.props.type}</span>
+        <span className="year"> {this.props.year}</span>
         <span className="city"> {this.props.city}</span>
         <span className="event"> ({this.props.event})</span>
         <span className="category"> {this.props.category}</span>
